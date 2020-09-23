88f7077c649a5ded381da6bc246e81d3b7d03b2c
Emma Limoli <EmmaLimoli024@gmail.com>
2020-09-23 13:24:15 -0600

    trying to pull file

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,12 +13,12 @@
 // display the key pair from the JSON metadata
 // update the plots any time that a new sample is selected
 
-// d3.json("./data/samples.json".then((metadata) => {
-//     console.log(metadata [0]);
-// }));
+d3.json("./static/js/samples.json", function(metadata) {
+    console.log(metadata [0]);
+});
 
 // function to call on samples.json and pulling out top 10
-function dataSamples (id) {
+function dataSamples(id) {
     d3.json("./static/js/samples.json").then((data) => {
         console.log(data);
     // create bar horo chart, drop down menu, display top 10 OTUs
@@ -28,8 +28,8 @@ function dataSamples (id) {
     // filter out top 10 OTUs
         var filterOtus = data.samples.filter(otus => otus.id.toString() === id, [0]);
         console.log(samples);
-    // sort and slice out top 10
-        var sort = filterOtus.reverse()
+    // sort and slice out top 10 use sample_values
+        var sort = filterOtus.sample_values.reverse()
         console.log(sort);
     // slice
         var slice = sort.slice(0,10)
